13e454922c62ae97d51ba6c10627f7ab3510ab8d
bibeknayak007 <bibeknayak.078@kathford.edu.np>
2026-02-26 16:23:09 +0545

    ..

--- backend/api/index.js
+++ backend/api/index.js
@@ -9,6 +9,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
 /* ===============================
    MongoDB Connection
 ================================= */
