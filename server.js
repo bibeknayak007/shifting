4f22b16b1998264777635e5a00013e9813174241
bibeknayak007 <bibeknayak.078@kathford.edu.np>
2026-02-26 14:07:12 +0545

    updated gitignore

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ app.use(express.json());
 app.use(cors());
 
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 
 // MongoDB Connection
 
